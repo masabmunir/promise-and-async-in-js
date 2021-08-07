fbafb7133d0e4a65878c64056fbe4972ae72c712
Masab <Masabk214@gmail.com>
2021-08-07 05:00:01 -0700

    promise in js

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log("working");
+// console.log("working");
+
+function dellAvailable() {
+    return false;
+}
+
+function hpAvailable() {
+    return true;
+}
+
+function buylaptop() {
+    return new Promise((resolve, reject) => {
+        if (this.dellAvailable()) {
+            return setTimeout(() => {
+                resolve('buy dell laptop');
+            }, 3000);
+        } else if (this.hpAvailable()) {
+           return setTimeout(() => {
+                resolve('buy hp laptop');
+            }, 3000);
+        } else {
+            reject("don't buy anthing");
+        }
+    });
+}
+
+buylaptop().then(res=>{
+    console.log(res)
+}).catch(res=>{
+    console.log(res)
+})
